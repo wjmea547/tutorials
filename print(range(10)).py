eb565bea43064df15d37e972e359e7fc7e512aa0
wjmea547 <wm547@georgetown.edu>
2024-07-10 16:01:08 -0400

    Add files via upload

---
+++ print(range(10)).py
@@ -0,0 +1,21 @@
+groceryList = ["sardines", "bananas", "cereal", "wasabi peas", "peanuts", "coffee"]
+"""
+for i in range(len(groceryList)):
+    print(groceryList[i])"""
+numberList = [ 2, 4, 6, 8 ,10 ]
+"""
+for i in range(len(numberList)):
+    print(numberList[i])"""
+"""
+sum = 0
+numberList = [ 2, 4, 6, 8 ,10 ]
+for i in range(len(numberList)):
+    sum = ((numberList[i]) + sum) #sum += numberList[i]
+print(sum)
+average = sum / len(numberList)
+print(average)
+
+for item in groceryList: #for-each
+    print(item)"""
+for item in numberList:
+    print(item)
